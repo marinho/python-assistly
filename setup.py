16249ae67a0dc9d8d916f57b518cfc61ec12282e
Caleb Smith <csmith@caktusgroup.com>
2012-02-07 13:02:31 -0500

    Patched setup to remove circular dependency problems

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-import assistly
 import os
+import re
+
+__version__ = re.search("__version__\s*=\s*'(.*)'", open('assistly/__init__.py').read(), re.M).group(1)
+assert __version__
+__author__ = re.search("__author__\s*=\s*'(.*)'", open('assistly/__init__.py').read(), re.M).group(1)
+assert __author__
+__license__ = re.search("__license__\s*=\s*'(.*)'", open('assistly/__init__.py').read(), re.M).group(1)
+assert __license__
 
 # Downloads setuptools if not find it before try to import
 try:
@@ -40,12 +47,12 @@ for dirpath, dirnames, filenames in os.walk(assistly_dir):
 
 setup(
     name='python-assistly',
-    version=assistly.__version__,
+    version=__version__,
     #url='',
-    author=assistly.__author__,
-    license=assistly.__license__,
+    author=__author__,
+    license=__license__,
     packages=packages,
     data_files=data_files,
-    requires=['simplejson','oauth2','httplib2'],
+    install_requires=['distribute','simplejson==2.2.1','oauth2==1.5.170','httplib2==0.7.1'],
+    setup_requires=['httplib2==0.7.1'],
     )
-
